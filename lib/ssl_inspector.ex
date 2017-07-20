c1c1e7be8647b7d4519947d4db15d11b4f38e4f5
Mike Clarke <mike@lambdafunctions.com>
2017-07-20 16:36:10 +0100

    Initial commit

---
+++ lib/ssl_inspector.ex
@@ -0,0 +1,18 @@
+defmodule SslInspector do
+  @moduledoc """
+  Documentation for SslInspector.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SslInspector.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ssl_inspector_test.exs
@@ -0,0 +1,8 @@
+defmodule SslInspectorTest do
+  use ExUnit.Case
+  doctest SslInspector
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
